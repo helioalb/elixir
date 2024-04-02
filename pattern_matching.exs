2183903485c15faaa9baa6ba5515a5e88d7c3760
Helio Albano de Oliveira <helioalb@gmail.com>
2024-04-01 22:20:58 -0300

    Add example of list match

--- pattern_matching.exs
+++ pattern_matching.exs
@@ -1,5 +1,20 @@
 a = 1
 1 = a
 
-# Error
-2 = a
+IO.puts(a)
+
+list = [1, 2, 3]
+[a, b , c] = list
+
+IO.puts(a)
+IO.puts(b)
+IO.puts(c)
+
+list = [1, 2, [3, 4]]
+[a, b , c] = list
+
+[d, e] = c
+IO.puts(a)
+IO.puts(b)
+IO.puts(d)
+IO.puts(e)
